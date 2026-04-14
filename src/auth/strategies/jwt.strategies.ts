5a1267be17ca84928ed2588144b18895af88928a
13bang <afif.putra224@gmail.com>
2026-04-14 09:27:09 +0700

    update

--- src/auth/strategies/jwt.strategies.ts
+++ src/auth/strategies/jwt.strategies.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express'; // Pastikan import Request dari express
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest:
+      jwtFromRequest: ExtractJwt.fromExtractors([
         ExtractJwt.fromAuthHeaderAsBearerToken(),
+        
+        (request: Request) => {
+          return request?.query?.token as string || null;
+        },
+      ]),
 
       ignoreExpiration: false,
 
-      secretOrKey:
-        process.env.JWT_SECRET || 'SUPER_SECRET_KEY',
+      secretOrKey: process.env.JWT_SECRET || 'SUPER_SECRET_KEY',
     });
   }
 
